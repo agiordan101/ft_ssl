297903fad65c40027c4abeb602e69d336eba0ca4
agiordan101 <agd611@gmail.com>
2022-03-07 13:17:26 +0100

    End reword des unpadding

--- srcs/algorithms/padding.c
+++ srcs/algorithms/padding.c
@@ -75,63 +75,35 @@ Long_64bits des_padding(Mem_8bits *bloc, Long_64bits blocByteSz)
 
 void        des_unpadding(Long_64bits *lastbloc, int *ptByteSz)
 {
+    Mem_8bits   validpadd = 1;
     Mem_8bits   lastbyte = (*lastbloc >> 56) & 0xff;
 
-    fprintf(stderr, "\nlastbloc: %lx\tlastbyte: %x\tptByteSz: %d\n", *lastbloc, lastbyte, *ptByteSz);
-    // printf("lastbyte : %x\n", lastbyte);
-    if (lastbyte == 0x08)
-        *ptByteSz -= LONG64_byteSz;
-    else if (0x01 <= lastbyte && lastbyte <= 0x07)
+    // fprintf(stderr, "\nlastbloc: %lx\tlastbyte: %x\tptByteSz: %d\n", *lastbloc, lastbyte, *ptByteSz);
+    if (lastbyte <= 0x08) // Padding found
     {
-        *lastbloc = *lastbloc & (((Long_64bits)1 << (64 - lastbyte * 8)) - 1); //Remove padding
-        *ptByteSz -= lastbyte;
+        // Search all padding bytes
+        for (int i = 0, rot = 56; i < lastbyte; i++, rot -= 8)
+            if (((*lastbloc >> rot) & 0xff) != lastbyte)  // Invalid padding
+            {
+                validpadd = 0;
+                break ;
+            }
     }
     else // Padding not found
+        validpadd = 0;
+
+    if (validpadd)
     {
-        if (~ssl.flags & nopad)
-            ft_ssl_error("Bad decrypt: No padding found in decrypted data.\n");
-        return ;
+        if (ssl.flags & nopad)
+            flag_error("-nopad", "-nopad is conflicting with padding found in decrypted data.");
+        if (lastbyte == 0x08)
+            *ptByteSz -= LONG64_byteSz;
+        else
+        {
+            *lastbloc = *lastbloc & (((Long_64bits)1 << (64 - lastbyte * 8)) - 1); //Remove padding
+            *ptByteSz -= lastbyte;
+        }
     }
-    // Padding found
-
-    if (ssl.flags & nopad)
-        flag_error("-nopad", "-nopad is conflicting with padding found in decrypted data.");
+    else if (~ssl.flags & nopad)
+        ft_ssl_error("Bad decrypt: No padding found in decrypted data.\n");
 }
-
-// void        des_unpadding(Long_64bits *lastbloc, int *ptByteSz)
-// {
-//     Mem_8bits   validpadd = 1;
-//     Mem_8bits   lastbyte = (*lastbloc >> 56) & 0xff;
-
-//     fprintf(stderr, "\nlastbloc: %lx\tlastbyte: %x\tptByteSz: %d\n", *lastbloc, lastbyte, *ptByteSz);
-//     if (lastbyte <= 0x08) // Padding found
-//     {
-//         // Search all padding bytes
-//         for (int i = 0, rot = 56; i < lastbyte; i++, rot -= 8)
-//             if ((*lastbloc >> rot) & 0xff != lastbyte)  // Invalid padding
-//             {
-//                 validpadd = 0;
-//                 break ;
-//             }
-//     }
-//     else // Padding not found
-//         validpadd = 0;
-
-//     if (validpadd)
-//     {
-//         if (ssl.flags & nopad)
-//             flag_error("-nopad", "-nopad is conflicting with padding found in decrypted data.");
-//         if (lastbyte == 0x08)
-//             *ptByteSz -= LONG64_byteSz;
-//         else
-//         {
-//             *lastbloc = *lastbloc & (((Long_64bits)1 << (64 - lastbyte * 8)) - 1); //Remove padding
-//             *ptByteSz -= lastbyte;
-//         }
-//     }
-//     else
-//     {
-//         if (~ssl.flags & nopad)
-//             ft_ssl_error("Bad decrypt: No padding found in decrypted data.\n");
-//     }
-// }
